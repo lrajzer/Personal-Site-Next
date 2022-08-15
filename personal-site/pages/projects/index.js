109305a2a80d0a200cafb50f0760155cc9f5b800
MichalRajzer <michal.rajzer03@gmail.com>
2022-08-15 21:50:43 +0200

    first steps for projects tab

---
+++ personal-site/pages/projects/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+export default function Projects() {
+    return (
+      <Layout>
+        <embed
+          src="http://www.example.com"
+        />
+      </Layout>
+    );
+}
