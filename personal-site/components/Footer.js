d6057c2c015cf8dbd84c0987691b67f8b39d5bda
MichalRajzer <michal.rajzer03@gmail.com>
2022-08-16 09:10:44 +0200

    Added footer

--- personal-site/components/Footer.js
+++ personal-site/components/Footer.js
@@ -1,6 +1,32 @@
 import styles from "../styles/Footer.module.css";
 import Link from "next/link";
 
-export default function Navbar(props) {
-  return <footer></footer>;
+export default function Footer(props) {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        <Link href="">
+          <a className={styles.link}> My resume</a>
+        </Link>
+        <Link href="">
+          <a className={styles.link}>My CV</a>
+        </Link>
+        <Link href="">
+          <a className={styles.link}>Privacy policy</a>
+        </Link>
+        <Link href="/">
+          <a className={`${styles.link} ${styles.noMobile}`}>About me</a>
+        </Link>
+        <Link href="">
+          <a className={`${styles.link} ${styles.noMobile}`}>Contact me</a>
+        </Link>
+        <Link href="/blog">
+          <a className={`${styles.link} ${styles.noMobile}`}>My blog</a>
+        </Link>
+        <Link href="/projects">
+          <a className={`${styles.link} ${styles.noMobile}`}>My projects</a>
+        </Link>
+      </div>
+    </footer>
+  );
 }
