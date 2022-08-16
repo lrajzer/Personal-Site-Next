adbbbabf08308aff002a94d9fc23541ec197c336
MichalRajzer <michal.rajzer03@gmail.com>
2022-08-16 08:43:48 +0200

    Added the first version of the projects page

--- personal-site/pages/projects/index.js
+++ personal-site/pages/projects/index.js
@@ -1,11 +1,51 @@
 import Layout from "../../components/Layout";
+import styles from "../../styles/Projects.module.css";
+import Link from "next/link";
 
 export default function Projects() {
-    return (
-      <Layout>
-        <embed
-          src="http://www.example.com"
-        />
-      </Layout>
-    );
+  return (
+    <Layout>
+      <div className={styles.mainContent}>
+        <h1 className={styles.projectsTitle}>My projects</h1>
+        <table className={styles.projectsTable}>
+          <caption className={styles.projectsCaption}>
+            It's best to check out each project by clicking on it&apos;s name.
+            <br />
+            To explore more projects from each category, click on the language.
+          </caption>
+          <tbody>
+            <tr>
+              <th className={styles.projectsHeading}>
+                <Link href="/projects/python"> Python </Link>
+              </th>
+              <th className={styles.projectsHeading}>
+                <Link href="/projects/javascript">Javascript</Link>
+              </th>
+              <th className={styles.projectsHeading}>
+                <Link href="/projects/arduino">Arduino</Link>
+              </th>
+            </tr>
+            <tr>
+              <td className={styles.project}></td>
+              <td className={styles.project}>
+                <a href="https://mypronouns.tech" target="_blank">
+                  <h2 className={styles.projectTitle}>MyPronouns.Tech</h2>
+                  <p className={styles.projectDescription}>
+                    This is a simple webpage and API desinged to help people use
+                    correct pronouns instead of the generic they/them
+                  </p>
+                  <embed
+                    type="text/html"
+                    src="https://mypronouns.tech"
+                    className={styles.embededMPT}
+                  />
+                </a>
+              </td>
+              <td className={styles.project}></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  );
 }
