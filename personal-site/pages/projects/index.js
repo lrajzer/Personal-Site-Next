0ede3d3613ad4238fd236036f86c48201ce95dbe
MichalRajzer <michal.rajzer03@gmail.com>
2022-08-16 09:41:11 +0200

    refactored and made design responsive

--- personal-site/pages/projects/index.js
+++ personal-site/pages/projects/index.js
@@ -7,45 +7,47 @@ export default function Projects() {
     <Layout>
       <div className={styles.mainContent}>
         <h1 className={styles.projectsTitle}>My projects</h1>
-        <table className={styles.projectsTable}>
-          <caption className={styles.projectsCaption}>
-            It&apos;s best to check out each project by clicking on it&apos;s
-            name.
-            <br />
-            To explore more projects from each category, click on the language.
-          </caption>
-          <tbody>
-            <tr>
-              <th className={styles.projectsHeading}>
-                <Link href="/projects/python"> Python </Link>
-              </th>
-              <th className={styles.projectsHeading}>
-                <Link href="/projects/javascript">Javascript</Link>
-              </th>
-              <th className={styles.projectsHeading}>
-                <Link href="/projects/arduino">Arduino</Link>
-              </th>
-            </tr>
-            <tr>
-              <td className={styles.project}></td>
-              <td className={styles.project}>
-                <a href="https://mypronouns.tech" target="_blank" rel='noreferrer'>
-                  <h2 className={styles.projectTitle}>MyPronouns.Tech</h2>
-                  <p className={styles.projectDescription}>
-                    This is a simple webpage and API desinged to help people use
-                    correct pronouns instead of the generic they/them
-                  </p>
-                  <embed
-                    type="text/html"
-                    src="https://mypronouns.tech"
-                    className={styles.embededMPT}
-                  />
-                </a>
-              </td>
-              <td className={styles.project}></td>
-            </tr>
-          </tbody>
-        </table>
+        <h2 className={styles.projectsCaption}>
+          It&apos;s best to check out each project by clicking on it&apos;s
+          name.
+          <br />
+          To explore more projects from each category, click on the language.
+        </h2>
+        <section className={styles.projectsTable}>
+          <div className={styles.row}>
+            <div className={styles.project}>
+              <Link href="/projects/python">
+                <a className={styles.projectsHeading}>Python</a>
+              </Link>
+            </div>
+            <div className={styles.project}>
+              <Link href="/projects/javascript">
+                <a className={styles.projectsHeading}>Javascript</a>
+              </Link>
+              <a
+                href="https://mypronouns.tech"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <h2 className={styles.projectTitle}>MyPronouns.Tech</h2>
+                <p className={styles.projectDescription}>
+                  This is a simple webpage and API desinged to help people use
+                  correct pronouns instead of the generic they/them
+                </p>
+                <embed
+                  type="text/html"
+                  src="https://mypronouns.tech"
+                  className={styles.embededMPT}
+                />
+              </a>
+            </div>
+            <div className={styles.project}>
+              <Link href="/projects/arduino">
+                <a className={styles.projectsHeading}>Arduino</a>
+              </Link>
+            </div>
+          </div>
+        </section>
       </div>
     </Layout>
   );
