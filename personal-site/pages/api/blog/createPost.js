53fb1d8b7336d78b5a3383f378929421806d6483
MichalRajzer <michal.rajzer03@gmail.com>
2022-08-20 04:59:23 +0200

    Addded the prelim Blog API routes

---
+++ personal-site/pages/api/blog/createPost.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * @param {import("next/types").NextApiRequest} req
+ * @param {import("next/types").NextApiResponse} res
+ */
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
+import connectDB from "../../../components/db/connectDB";
+import BlogPost from "../../../components/db/models/blogPost";
+
+export default withApiAuthRequired(async function addPost(req, res) {
+  const { uid, title, content } = req.query;
+  const { user } = getSession(req, res);
+  // console.log(user);
+  if (user.sub !== process.env.ADMINSUB)
+  {
+    res.status(401).json({ error: "unathorized" });
+    return 0;
+  }
+  if (!uid || !title || !content) {
+    // console.log("It should quit here");
+    res.status(400).json({
+      missing:
+        (uid ? " " : "uid ") +
+        (title ? " " : "title ") +
+        (content ? " " : "content "),
+    });
+    return 0;
+  }
+  await connectDB();
+  let existingPost = await BlogPost.findOne({ uid: uid });
+  // console.log(existingPost);
+  if (existingPost === null) {
+    let newPost = await BlogPost.create({
+      uid: uid,
+      title: title,
+      content: content,
+      dateCreated: new Date(),
+    });
+    res.status(200).json({ post: newPost });
+    return 0;
+  }
+  res.status(400).json({ error: "already exists" });
+});
---
+++ personal-site/pages/api/blog/deletePost.js
@@ -0,0 +1,33 @@
+/**
+ *
+ * @param {import("next/types").NextApiRequest} req
+ * @param {import("next/types").NextApiResponse} res
+ */
+
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
+import connectDB from "../../../components/db/connectDB";
+import BlogPost from "../../../components/db/models/blogPost";
+
+export default withApiAuthRequired (async function addPost(req, res) {
+  const { uid } = req.query;
+  const { user } = getSession(req, res);
+  if (!uid) {
+    res.status(400).json({ missing: "uid" });
+    return 0;
+  }
+  if (user.sub !== process.env.ADMINSUB) {
+    res.status(401).json({ error: "unathorized" });
+    return 0;
+  }
+  await connectDB();
+  try {
+  BlogPost.deleteOne({ uid: uid }, function (err) {
+    if (err) return handleError(err);
+  });
+  res.status(200).json({sucess: true})
+  }
+  catch (err) {
+    console.log(err)
+    res.status(400).json(err);
+  }
+});
