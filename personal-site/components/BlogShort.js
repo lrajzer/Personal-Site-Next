e4429ada6fc20752a8bfb5a707a2653fad07bd67
MichalRajzer <michal.rajzer03@gmail.com>
2022-08-25 01:25:14 +0200

    reverted changes as previous changes broke functionality

--- personal-site/components/BlogShort.js
+++ personal-site/components/BlogShort.js
@@ -1,18 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+
 import Link from "next/link";
 import styles from "../styles/Blogs.module.css";
 import TagRemover from "../utils/TagRemover";
-import Image from "next/Image";
 
 export default function BlogShort(props) {
   let maxLength = 303;
-  if(props.maxLength !== undefined)
-  {
-    maxLength = props.maxLength; 
+  if (props.maxLength !== undefined) {
+    maxLength = props.maxLength;
   }
-  
+
   const thisBlog = props.blog;
   const image = thisBlog.coverImage ? (
-    <Image
+    <img
       src={thisBlog.coverImage.url}
       alt={thisBlog.coverImage.alt}
       className={styles.CoverImage}
@@ -23,17 +23,17 @@ export default function BlogShort(props) {
   const date = thisBlog.dateModified
     ? new Date(thisBlog.dateModified)
     : new Date(thisBlog.dateCreated);
-  
+
   let shortDesc = TagRemover(thisBlog.content);
-  
+
   if (shortDesc.length > maxLength) {
-    shortDesc = shortDesc.slice(0, maxLength-3) + "...";
+    shortDesc = shortDesc.slice(0, maxLength - 3) + "...";
   }
 
   return (
     <li className={styles.BlogLi}>
       {image}
-      <div style={{width:`100%`}}>
+      <div style={{ width: `100%` }}>
         <div className={styles.Titular}>
           <div>
             <h3>
@@ -44,11 +44,13 @@ export default function BlogShort(props) {
           </div>
           <div className={styles.Meta}>
             <p className={styles.Author}>
-              <Link href="/">{thisBlog.author}</Link>
+              <Link href="/">Michał Rajzer</Link>
             </p>
-            {<time dateTime={date.toISOString()} className={styles.Date}>
-              {date.toLocaleDateString("en-GB")}
-            </time>}
+            {
+              <time dateTime={date.toISOString()} className={styles.Date}>
+                {date.toLocaleDateString("en-GB")}
+              </time>
+            }
           </div>
         </div>
         <p className={styles.Description}>
