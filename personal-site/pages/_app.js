8944931ce93cf87e5a15df68950f94665157c758
MichalRajzer <michal.rajzer03@gmail.com>
2022-09-07 01:51:40 +0200

    Added ReCAPTCHA to whole site

--- personal-site/pages/_app.js
+++ personal-site/pages/_app.js
@@ -1,12 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <GoogleReCaptchaProvider
+      reCaptchaKey="6Lcm47khAAAAAEUsXWelWIYiAS9lJlOxy2JLJM4G"
+      scriptProps={{
+        async: false,
+        defer: false,
+        appendTo: "head",
+        nonce: undefined,
+      }}
+    >
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </GoogleReCaptchaProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
