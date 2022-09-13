1317f6778576d2900f61e70b05299a848318338f
MichalRajzer <michal.rajzer03@gmail.com>
2022-09-13 03:40:42 +0200

    Added language switcher

--- personal-site/components/Layout.js
+++ personal-site/components/Layout.js
@@ -2,8 +2,12 @@ import Navbar from "./Navbar";
 import styles from "../styles/Layout.module.css";
 import Head from "next/head";
 import Footer from "./Footer";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Layout(props) {
+  const { asPath } = useRouter();
+  console.log(asPath)
   return (
     <>
       <Head>
@@ -35,8 +39,28 @@ export default function Layout(props) {
         />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
+      <div className={styles.LangSwitch}>
+        <h6>
+          {props.pl ? (
+            <Link href={`${asPath.replace("/pl", "/")}`}>Pl</Link>
+          ) : (
+            <Link href={`/pl/${asPath}`}>En</Link>
+          )}
+        </h6>
+        <span>
+          {props.pl ? (
+            <Link href={`${asPath.replace("/pl", "/")}`}>
+              Kliknij tu aby zmienić język
+            </Link>
+          ) : (
+            <Link href={`/pl/${asPath}`}>
+              Click here to change the language
+            </Link>
+          )}
+        </span>
+      </div>
       <main>
-        <Navbar inBrackets={props.inBrackets} pl={props.pl}/>
+        <Navbar inBrackets={props.inBrackets} pl={props.pl} />
         <div className={styles.content}>{props.children}</div>
         <Footer />
       </main>
