c39b2f23ceb5ed0f29c5341121af0779c735591c
Michal Rajzer <michal.rajzer03@gmail.com>
2022-10-28 12:29:15 +0200

    Added first python project

--- personal-site/pages/projects/python.js
+++ personal-site/pages/projects/python.js
@@ -1,5 +1,43 @@
 import Layout from "../../components/Layout";
+import Image from "next/image";
+import styles from "../../styles/Projects.module.css";
 
 export default function Python() {
-  return <Layout>Hello Python!</Layout>;
+  return (
+    <Layout>
+      <h1 className={styles.projectsTitle}>My Python Projects</h1>
+      <ul className={styles.projectList}>
+        <li style={{ marginTop: "5vh", marginBottom: "5vh" }}>
+          <h3 className={styles.projectsHeading}>Sudoku solver</h3>
+          <div className={styles.projectDetails}>
+            <div className={styles.projectImgWrapper}>
+              <Image
+                src={"/imgs/solver.png"}
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+            <p
+              className={styles.projectDescription}
+              style={{
+                textAlign: "justify",
+                marginLeft: "1vw",
+                width: "calc(100% - 1vw)",
+              }}
+            >
+              I created a simple sudoku solver in Python 3 using the Pygame
+              library for the graphical side. I implemented the recursive
+              backtracking algorithm on my own, as I had tried to write this
+              code as quickly as I could, and it took me about an hour to create
+              the whole solution, from the algorithm design to the finished
+              working product. The algorithm works in 3 parts. First it finds
+              the next cell to fill, then it generates the possible values, and
+              then it checks the values. In the event that no values fit, it
+              then backtracks change the previous values.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </Layout>
+  );
 }
