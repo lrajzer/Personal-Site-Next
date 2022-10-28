2a13a0a2a8e8d7313716c4e789b21ad3cfead8f9
Michal Rajzer <michal.rajzer03@gmail.com>
2022-10-28 12:33:15 +0200

    Added link to CV

--- personal-site/components/Footer.js
+++ personal-site/components/Footer.js
@@ -1,16 +1,16 @@
 import styles from "../styles/Footer.module.css";
 import Link from "next/link";
 
-export default function Footer({pl}) {
+export default function Footer({ pl }) {
   return (
     <footer className={styles.footer}>
       <div className={styles.links}>
         <Link href="">
           <a className={styles.link}>Resume</a>
         </Link>
-        <Link href="">
-          <a className={styles.link}>CV</a>
-        </Link>
+        <a className={styles.link} href="/docs/CV_def.pdf">
+          CV
+        </a>
         <Link href="https://www.iubenda.com/privacy-policy/15454240" passHref>
           <a target="_blank" rel="noopener noreferrer" className={styles.link}>
             {pl ? "Polityka prywatności" : "Privacy policy"}
@@ -31,7 +31,8 @@ export default function Footer({pl}) {
         </Link>
         <Link href={pl ? "/projekty" : "/projects"}>
           <a className={`${styles.link} ${styles.noMobile}`}>
-            {pl ? "Projekty" : "Projects"}</a>
+            {pl ? "Projekty" : "Projects"}
+          </a>
         </Link>
       </div>
     </footer>
