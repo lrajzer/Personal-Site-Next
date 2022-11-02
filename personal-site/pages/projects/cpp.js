5700d7dd4b9d9849617f4989b787e5b26a63f66b
Michal Rajzer <michal.rajzer03@gmail.com>
2022-11-02 18:14:09 +0100

    Made a goof

--- personal-site/pages/projects/cpp.js
+++ personal-site/pages/projects/cpp.js
@@ -2,7 +2,7 @@ import Layout from "../../components/Layout";
 import { useEffect } from "react";
 
 export default function Cpp() {
-  alert("This part of my site is still under construction!");
+  useEffect(() => alert("This part of my site is still under construction!"));
   return (
     <Layout>
       <h1>C++ Projects</h1>
