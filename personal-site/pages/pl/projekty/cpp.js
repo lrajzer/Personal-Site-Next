fa5cee62fe3e301548993a671a388f44f09b7867
Michal Rajzer <michal.rajzer03@gmail.com>
2022-11-02 18:17:14 +0100

    Added WIP msg to pl c++ projects

--- personal-site/pages/pl/projekty/cpp.js
+++ personal-site/pages/pl/projekty/cpp.js
@@ -1,9 +1,11 @@
 import Layout from "../../../components/Layout";
 
 export default function cpp() {
+  useEffect(() => alert("Ta część strony jest nadal niedokończona!"));
   return (
     <Layout pl={true}>
       <h1>Projekty C++</h1>
+      <p>WIP</p>
     </Layout>
   );
 }
