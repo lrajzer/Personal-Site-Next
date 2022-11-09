0003380ea01fc13901b51d98aa103918b642fd08
Michal Rajzer <michal.rajzer03@gmail.com>
2022-11-09 13:25:14 +0100

    created jam helper page

---
+++ personal-site/pages/hidden/jam-helper.js
@@ -0,0 +1,11 @@
+import styles from "../../styles/Home.module.css";
+import Layout from "../../components/Layout";
+import Link from "next/link";
+
+export default function Hidden() {
+  return (
+    <Layout title="Gamejam helper" isMonoLang={true}>
+      <h1>Gamejam helper</h1>
+    </Layout>
+  );
+}
