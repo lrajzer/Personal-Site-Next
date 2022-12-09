278191a3799e9cb84ddebce15aab73718358b335
Michal Rajzer <michal.rajzer03@gmail.com>
2022-12-10 00:59:13 +0100

    Added Google Analytics

--- personal-site/pages/_app.js
+++ personal-site/pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,6 +14,22 @@ function MyApp({ Component, pageProps }) {
         nonce: undefined,
       }}
     >
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-3VNMKTW3N3"
+      />
+
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());gtag('config', 'G-3VNMKTW3N3', 
+           {page_path: window.location.pathname,});`,
+        }}
+      />
       <UserProvider>
         <Component {...pageProps} />
       </UserProvider>
