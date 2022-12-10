7663d39e155cf56e0b84cd6fe3688299d5df3c0a
Michal Rajzer <michal.rajzer03@gmail.com>
2022-12-10 01:10:35 +0100

    Fixed small mistake

--- personal-site/pages/_app.js
+++ personal-site/pages/_app.js
@@ -29,10 +29,12 @@ function MyApp({ Component, pageProps }) {
     >
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
+        id="gtag"
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
+        id="gtag2"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
