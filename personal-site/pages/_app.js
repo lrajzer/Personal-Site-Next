56356781d8734a23e4f7392188beed10e8d8395a
Michal Rajzer <michal.rajzer03@gmail.com>
2022-12-10 01:07:24 +0100

    Fully added GA

--- personal-site/pages/_app.js
+++ personal-site/pages/_app.js
@@ -2,8 +2,21 @@ import "../styles/globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../components/GA";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey="6Lcm47khAAAAAEUsXWelWIYiAS9lJlOxy2JLJM4G"
@@ -14,20 +27,22 @@ function MyApp({ Component, pageProps }) {
         nonce: undefined,
       }}
     >
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
         strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-3VNMKTW3N3"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
-
       <Script
-        id="google-analytics"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());gtag('config', 'G-3VNMKTW3N3', 
-           {page_path: window.location.pathname,});`,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
         }}
       />
       <UserProvider>
