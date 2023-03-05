32b1b0e412b9690336d708a00a9cf1305c1afc0f
Michal Rajzer <114916818+michalRajzer03@users.noreply.github.com>
2023-03-05 12:57:25 +0100

    fixed me being stupid

--- personal-site/pages/blog/create.js
+++ personal-site/pages/blog/create.js
@@ -1,15 +1,14 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useRouter } from "next/router";
 import CEBlogForm from "../../components/CEBlogForm";
 import Layout from "../../components/Layout";
 
 export default withPageAuthRequired(function CreateBlog({ user }) {
+  const router = useRouter();
   if (user !== process.env.ADMINSUB) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
+    console.log("Unauthorized user: " + user);
+    router.push("/api/auth/logout");
+    return <></>;
   }
   return (
     <Layout isMonoLang={true}>
