2972bc1cbf2c483da1e2ba74d436dcc1700164cc
Michal Rajzer <michal.rajzer03@gmail.com>
2023-03-10 12:42:41 +0100

    Added styling

--- personal-site/pages/hidden/jam-helper.js
+++ personal-site/pages/hidden/jam-helper.js
@@ -1,4 +1,4 @@
-import styles from "../../styles/Home.module.css";
+import styles from "../../styles/JamHelper.module.css";
 import { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
 
@@ -19,61 +19,87 @@ export default function Hidden() {
 
   return (
     <Layout title="Gamejam helper" isMonoLang={true}>
-      <h1>Gamejam helper</h1>
-      <p>
-        This is a small gamejam helper that works in the way ad-libs work by
-        filling the gaps with random words from the word base. To use it, enter
-        the phrase in the first input box with gaps surrounded with brackets,
-        for example: Make a game using {"{mechanic}"} in the theme of{" "}
-        {"{theme}"}. Then You can add words to each group. When You're done, You
-        can use the button generate to get Your filled out sentence.
-      </p>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          name="sentence"
-          id="sentence"
-          placeholder="Please enter the phrase with the {gaps}"
-          onChange={(e) => setSentence(e.target.value)}
-        />
-        <input
-          type="text"
-          name="word"
-          id="word"
-          placeholder="Please enter the word"
-          onChange={(e) => setWord(e.target.value)}
-        />
-        <button
-          onClick={(e) => {
-            if (word === null) {
-              alert("Please fill in the word >:(");
-              return;
-            }
-            if (activeGroup === null) {
-              alert("Please select a group >:(");
-              return;
-            }
-            setWords(words.push([word, activeGroup]));
-            setWord(null);
+      <div className={styles.content}>
+        <h1 className={styles.heading}>Gamejam helper</h1>
+        <p className={styles.desc}>
+          This is a small gamejam helper that works in the way ad-libs work by
+          filling the gaps with random words from the word base. To use it,
+          enter the phrase in the first input box with gaps surrounded with
+          brackets, for example: Make a game using {"{mechanic}"} in the theme
+          of {"{theme}"}. Then You can add words to each group. When You're
+          done, You can use the button generate to get Your filled out sentence.
+        </p>
+        <form
+          onSubmit={(e) => {
+            console.log("Submited!");
+            e.preventDefault();
+            generate();
           }}
+          className={styles.inForm}
         >
-          Add word
-        </button>
-        <select
-          name="emptySpace"
-          id="emptySpace"
-          placeholder="Group"
-          onChange={(e) => {
-            setActiveGroup(e.target.value);
-          }}
-        >
-          <option value="placeholder" disabled>
-            Please select a group
-          </option>
-        </select>
-        <button onClick={generate()}>Generate!</button>
-      </form>
-      <div>Word cloud</div>
+          <input
+            type="text"
+            name="sentence"
+            id="sentence"
+            placeholder="Please enter the phrase with the {gaps}"
+            onChange={(e) => setSentence(e.target.value)}
+            className={styles.sentenceIn}
+          />
+          <div className={styles.wordInDiv}>
+            <input
+              type="text"
+              name="word"
+              id="word"
+              placeholder="Please enter the word"
+              onChange={(e) => setWord(e.target.value)}
+              className={styles.wordIn}
+            />
+            <button
+              onClick={(e) => {
+                if (word === null) {
+                  alert("Please fill in the word >:(");
+                  return;
+                }
+                if (activeGroup === null) {
+                  alert("Please select a group >:(");
+                  return;
+                }
+                setWords(words.push([word, activeGroup]));
+                setWord(null);
+              }}
+              className={styles.wordButton}
+            >
+              Add word
+            </button>
+          </div>
+          <div className={styles.wordInDiv}>
+            <label htmlFor="actFunc" className={styles.label}>
+              Blank:&nbsp;
+            </label>
+            <select
+              name="emptySpace"
+              id="emptySpace"
+              placeholder="placeholder"
+              onChange={(e) => {
+                setActiveGroup(e.target.value);
+              }}
+              className={styles.groupSelector}
+            >
+              {groups ? (
+                <option value="placeholder" disabled>
+                  Please write a sentence to create blanks
+                </option>
+              ) : (
+                ""
+              )}
+            </select>
+          </div>
+          <button type="submit" className={styles.wordButton}>
+            Generate!
+          </button>
+        </form>
+        <div>Word cloud</div>
+      </div>
     </Layout>
   );
 }
