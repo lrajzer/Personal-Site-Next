460a6817501b4d4b85e3228ddcd4a498cffc5f52
Michał Rajzer <michal.rajzer03@gmail.com>
2023-05-18 15:24:46 +0200

    Set rescaptcha as  the featured project for JS

--- personal-site/pages/projects/index.js
+++ personal-site/pages/projects/index.js
@@ -3,8 +3,12 @@ import styles from "../../styles/Projects.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import pythonSolver from "../../public/imgs/solver.png";
+import { ResCaptchaElem } from "../../components/hidden/ResCaptcha";
+import connectDB from "../../components/db/connectDB";
+import ResCaptcha from "../../components/db/models/ResCaptcha";
+import ResCaptchaData from "../../components/db/models/ResCaptchaData";
 
-export default function Projects() {
+export default function Projects({ imgs, uid }) {
   return (
     <Layout
       title="My projects"
@@ -51,27 +55,79 @@ export default function Projects() {
               >
                 Javascript
               </Link>
-              <a href="https://ttfonte.com/" target="_blank" rel="noreferrer">
-                <h2 className={styles.projectTitle}>TTFonte.com</h2>
-                <p className={styles.projectDescription}>
-                  This is a simple webpage created by me for my current
-                  employer. It uses Next.JS as the framework.
-                </p>
-                <embed
-                  type="text/html"
-                  src="https://ttfonte.com/"
-                  className={styles.embededMPT}
-                />
-              </a>
+              <h2 className={styles.projectTitle}>ResCAPTCHA</h2>
+              <p className={styles.projectDescription}>
+                This is a simple ReCAPTCHA clone that I made for use in my
+                research projects, eg. if I need to process a lot of data for an
+                AI dataset.
+              </p>
+              <ResCaptchaElem imgs={imgs} uid={uid} />
             </div>
-            {/* <div className={styles.project}>
-              <Link href="/projects/cpp" className={styles.projectsHeading}>
-                C++
-              </Link>
-            </div> */}
           </div>
         </section>
       </div>
     </Layout>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  await connectDB();
+  let uid;
+  do {
+    uid =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15); // copilot whispered this and it works I guess?
+  } while ((await ResCaptcha.findOne({ uid: uid })) !== null);
+
+  let testPlates = await ResCaptchaData.aggregate([
+    { $match: { isSolved: true } },
+    { $sample: { size: 3 } },
+  ]);
+
+  let trainPlates = await ResCaptchaData.aggregate([
+    { $match: { isSolved: false } },
+    { $sample: { size: 3 } },
+  ]);
+
+  let testUrls = [];
+  let testVals = [];
+  let trainUrls = [];
+
+  testPlates.map((plate) => {
+    testUrls.push(plate.url);
+    testVals.push(plate.content);
+  });
+  trainPlates.map((plate) => {
+    trainUrls.push(plate.url);
+  });
+
+  console.log(testUrls, testVals, trainUrls);
+
+  const imgsServerside = {
+    testVals: testVals,
+    tests: testUrls,
+    train: trainUrls,
+  }; // TODO: for now this is hardcoded, change it so it generates some data on the fly...
+
+  const data = {
+    uid: uid,
+    testPlates: imgsServerside.testVals,
+    trainPlates: [],
+    testImgs: imgsServerside.tests,
+    trainImgs: imgsServerside.train,
+    dateCreated: new Date(),
+    solved: false,
+  };
+
+  const dbData = await ResCaptcha.create(data);
+  if (dbData === null) {
+    res.status(500).json({ error: "Internal server error" });
+    return 0;
+  }
+
+  const imgs = {
+    tests: imgsServerside.tests,
+    train: imgsServerside.train,
+  };
+  return { props: { imgs: imgs, uid: uid } };
+};
